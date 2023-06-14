55465a27daf7af3675e08dc996912d06259f544c
kaviarasan55555 <130632837+kaviarasan55555@users.noreply.github.com>
2023-06-14 18:01:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PricingSection from './PricingSection';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+const App = () => {
+  return (
+    <div>
+      {/* Other components */}
+      <PricingSection />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
